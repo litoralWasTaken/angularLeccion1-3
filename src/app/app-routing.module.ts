3166e8ccb82b7b3b273352c2c95ec314ab04468d
Rodrigo Garcia <litoralwastaken@gmail.com>
2023-06-27 17:41:21 +0200

    Lección 1-3: Instalación Angular, componentes y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurriculumComponent } from './pages/curriculum/curriculum.component';
+import { FormComponent } from './pages/form/form.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'form',
+    component: FormComponent
+  },
+  {
+    path: 'curriculum',
+    component: CurriculumComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
